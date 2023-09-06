722959a2927c3807071544ebba36496e5921aa70
sungho <10sungho27@gmail.com>
2023-09-06 22:48:04 +0800

    update2pages

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+//swiper
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto", // 參考解答解決換行問題了
+    allowTouchMove: false, // 參考解答加上
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    speed: 8000,
+  });
